ee40850f6342308776ca94574af92f7e692b766e
Null Reality <rchahine1@gmail.com>
2022-02-08 00:11:21 -0500

    made teleport lines disappear until using trigger

--- nowseum/Assets/Scripts/LocomotionController.cs
+++ nowseum/Assets/Scripts/LocomotionController.cs
@@ -2,24 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
-//using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class LocomotionController : MonoBehaviour {
 
-  //  public XRController leftTeleportRay;
-    // public XRController rightTeleportRay;
-    // public UnityEngine.XR.Interaction.Toolkit.InputHelpers.Button teleportActivationButton;
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
     public float activationThreshold = 0.1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    // Update is called once per frame
+    void Update() {
+
+      if(leftTeleportRay) {
+        leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+      }
+
+      if(rightTeleportRay) {
+        rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+      }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public bool CheckIfActivated(XRController controller) {
+      InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+      return isActivated;
     }
 }
