cbc122928bdc96ead65436e63a9daf7c596d5e03
Null Reality <rchahine1@gmail.com>
2022-02-07 22:13:21 -0500

    teleportation implemented

---
+++ nowseum/Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+//using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour {
+
+  //  public XRController leftTeleportRay;
+    // public XRController rightTeleportRay;
+    // public UnityEngine.XR.Interaction.Toolkit.InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
